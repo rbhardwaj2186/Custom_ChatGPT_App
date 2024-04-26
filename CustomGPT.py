0a035f665b211e13ff97198e6065a8e59729ee81
Rakshit Bhardwaj <143745073+rbhardwaj2186@users.noreply.github.com>
2024-04-25 21:44:54 -0500

    Update CustomGPT.py

--- CustomGPT.py
+++ CustomGPT.py
@@ -10,5 +10,20 @@ load_dotenv(find_dotenv(), override=True)
 
 st.set_page_config(
     page_title='Your Custom Assistant',
-    page_icon=""
+    page_icon=":home"
 )
+st.subheader('Your Custom ChatGPT :home')
+chat = ChatOpenAI(model_name='got-3.5-turbo', temperature=0.5)
+if 'messages' not in st.session_state:
+    st.session_state.messages = []
+
+with st.sidebar:
+    system_message = st.text_input(label='System role')
+    user_prompt = st.text_input(label='send a message')
+    if system_message:
+        if not any(isistance(x, SystemMessage) for x in st.session_state.messages):
+            st.session_state.messages.append(
+                SystemMessage(content=system_message)
+            )
+
+        # st.write(st.session)
